47392869a71cda383883c0e0601174dd604bc7e5
Chuck476 <cdierbach@towson.edu>
2024-01-26 19:06:55 -0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,31 @@
+// Practicum 8 (Even or Odd Program)
+// (TO COMPLETE)
+
+// This program will prompt the user for an integer value
+// and display whether it is even or odd.
+
+// REQUIRED OUTPUT:
+// The program MUST display the results exactly as shown below:
+// <n> is odd
+// <n> is even
+// --------------------------------------------------------------
+boolean odd;
+
+// (TO COMPLETE)
+
+
+
+
+
+
+
+
+
+
+
+
+// (MUST KEEP THESE LINES)
+if(odd)
+  System.out.println(n + " is odd");
+else
+  System.out.println(n + " is even");
