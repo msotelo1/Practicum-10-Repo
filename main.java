58a45098c8f92e0fe4c3fd9fc6a9c0d39eafde46
msotelo1 <158190815+msotelo1@users.noreply.github.com>
2024-02-06 04:02:43 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,31 +1,18 @@
-// Practicum 8 (Even or Odd Program)
-// (TO COMPLETE)
+import java.util.*;
 
-// This program will prompt the user for an integer value
-// and display whether it is even or odd.
-
-// REQUIRED OUTPUT:
-// The program MUST display the results exactly as shown below:
-// <n> is odd
-// <n> is even
-// --------------------------------------------------------------
-boolean odd;
-
-// (TO COMPLETE)
-
-
-
-
-
-
-
-
-
-
-
-
-// (MUST KEEP THESE LINES)
-if(odd)
-  System.out.println(n + " is odd");
-else
-  System.out.println(n + " is even");
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+    
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter in an Integer");
+    int n = input.nextInt();
+    
+    if(n % 2 != 0)
+      System.out.println(n + " is odd");
+    else
+      System.out.println(n + " is even");
+  }
+}
